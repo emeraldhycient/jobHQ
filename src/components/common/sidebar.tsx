d1f7f3ffa3297bc909c4c7a2ea15b064c19326da
igwezehycient86 <igwezehycient86@gmail.com>
2024-06-27 19:41:03 +0000

    feat: initialisation

---
+++ src/components/common/sidebar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { RiHomeLine } from "react-icons/ri";
+
+
+function Sidebar() {
+    return (
+        <section>
+            <ul>
+                <li>
+                    <RiHomeLine />
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <RiHomeLine />
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <RiHomeLine />
+                    <span>Dashboard</span>
+                </li>
+                <li>
+                    <RiHomeLine />
+                    <span>Dashboard</span>
+                </li>
+            </ul>
+        </section>
+    )
+}
+
+export default Sidebar
