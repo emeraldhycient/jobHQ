ae3567d6eb054985ca5d172f10770974182d30d3
emeraldhycient <igwezehycient86@gmail.com>
2024-07-02 20:09:58 +0200

    fix: error on vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "next-themes"
 import { FaSun, FaMoon, FaLaptopCode, FaGraduationCap, FaSuitcase, FaTwitter, FaLinkedin, FaFacebook, FaInstagram } from 'react-icons/fa';
 import HeroSection from '@/components/landing_page/hero';
 import Section2 from '@/components/landing_page/section2';
+import FeaturedJobs from '@/components/landing_page/featured_jobs';
 
 const Home = () => {
   const { setTheme } = useTheme()
@@ -18,7 +19,8 @@ const Home = () => {
     <div className="">
       <Header />
       <HeroSection />
-      <Section2/>
+      <Section2 />
+      <FeaturedJobs/>
     </div>
   );
 };
---
+++ src/components/landing_page/featured_jobs.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FeaturedJobs() {
+    return (
+        <section className='px-24 text-white pt-6 pb-10 mt-10'>
+            <div className="flex flex-col items-center justify-center">
+                <h4 className='text-xl font-semibold'>Explore Featured Jobs</h4>
+                <h6 className='text-[14px] font-medium my-3'>Explore available jobs and find your perfect and suitable job</h6>
+            </div>
+        </section>
+    )
+}
+
+export default FeaturedJobs
