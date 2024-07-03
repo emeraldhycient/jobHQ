392be174c8bea24b02f11fcc63d08325e406d041
emeraldhycient <igwezehycient86@gmail.com>
2024-07-03 16:42:40 +0200

    chore: adjusted a single style

--- src/components/landing_page/testimonies.tsx
+++ src/components/landing_page/testimonies.tsx
@@ -6,7 +6,7 @@ function Testimonies() {
       <section className='px-6 md:px-24 text-white py-10 mt-10 bg-gray-2'>
           <div className="flex flex-col items-center justify-center">
               <h4 className='text-xl font-semibold'>What our Clients Say</h4>
-              <h6 className='text-[14px] font-medium my-3 text-center w-[70%] lg:w-[65%]'>We have a reputation for helping companies and individuals around the world build wining teams and launch dream careers.</h6>
+              <h6 className='text-[14px] font-medium my-3 text-center w-[90%] md:w-[70%] lg:w-[65%]'>We have a reputation for helping companies and individuals around the world build wining teams and launch dream careers.</h6>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-2 gap-y-4 md:gap-x-4 md:gap-y-6 mt-6">
               <TestimonyCard/>
