80e9634ee9fcbc4c056ec7c0f377132d8688c13d
emeraldhycient <igwezehycient86@gmail.com>
2024-07-03 16:54:31 +0200

    feat: resposiveness on the hero section

--- src/components/landing_page/hero.tsx
+++ src/components/landing_page/hero.tsx
@@ -8,7 +8,7 @@ function HeroSection() {
     return (
         <section className='text-white lg:h-[90vh] w-screens px-6 md:px-20 lg:px-32 py-16 relative'>
             <div className="w-full h-full mb-4 md:mb-0 md:flex">
-                <div className="lg:px-20 md:w-[75%] lg:w-[50%] flex flex-col justify-center items-center">
+                <div className="lg:px-20 md:w-[90%] lg:w-[50%] flex flex-col justify-center items-center">
                     <h4 className='font-bold text-4xl'>Explore Opportunities, Upskill for Success, and Master Interviews</h4>
                     <Image src={"/icons/line.svg"} width={100} height={100} className='w-[100px]' alt='' />
                     <h6 className='text-sm font-semibold mt-8'>The All-in-One Platform for Job Seekers & Employers. Find Jobs, Upskill, Interview, and Hire Top Talent.</h6>
@@ -17,7 +17,7 @@ function HeroSection() {
                     <Image src={"/images/Image.png"} width={100} height={100} className='w-full h-full' alt='' />
                 </div>
             </div>
-            <section className='md:absolute bg-white p-3 md:w-[67%] lg:w-[66%] bottom-4 right-[250px] flex flex-col md:flex-row gap-3'>
+            <section className='lg:absolute bg-white p-3 md:w-[67%] lg:w-[66%] bottom-4 right-[250px] flex flex-col md:flex-row gap-3'>
                 <Input placeholder='Job Title, Skills or Company' />
                 <Input placeholder='City, State or Remote' />
                 <Button size={"lg"}>Search Jobs</Button>
