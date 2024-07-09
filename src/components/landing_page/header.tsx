23952af0a93e2021d2ea314f5684a4815e384102
emeraldhycient <igwezehycient86@gmail.com>
2024-07-09 19:06:44 +0200

    chore: upgraded the get started button

--- src/components/landing_page/header.tsx
+++ src/components/landing_page/header.tsx
@@ -29,9 +29,16 @@ function Header() {
                 <Button variant={'ghost'} asChild>
                   <Link href="/auth/login" className='text-white text-sm font-semibold'>Sign In</Link>
                 </Button>
-                <Button variant={'outline'} size={'lg'}>
-                  <Link href="/auth/create-account">Create Account</Link>
-                </Button>
+                <Link
+                  href="/auth/create-account"
+                  className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+                >
+                  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+                    {false ? 'Dashboard' : 'Get Started'}
+                  </span>
+                </Link>
+               
               </div>
               <div className="flex md:hidden">
                 <Disclosure.Button className="text-gray-400 hover:text-white focus:outline-none focus:text-white">
