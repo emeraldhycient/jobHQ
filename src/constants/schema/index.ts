7eff5e0d7a26ac6e53121936ab5ad738ff7a7e1f
emeraldhycient <igwezehycient86@gmail.com>
2024-07-19 18:48:33 +0200

    perf: updated the createAccountSchema

--- src/constants/schema/index.ts
+++ src/constants/schema/index.ts
@@ -1,13 +1,25 @@
 import Joi from 'joi';
 
 export const createAccountSchema = Joi.object({
-    email: Joi.string().email().when('userType', { is: 'User', then: Joi.required() }),
+    email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    name: Joi.string().when('userType', { is: 'User', then: Joi.required() }),
+    name: Joi.when('userType', {
+        is: 'User',
+        then: Joi.string().required(),
+        otherwise: Joi.string().optional(),
+    }),
     userType: Joi.string().valid('User', 'Employer').required(),
-    companyName: Joi.string().when('userType', { is: 'Employer', then: Joi.required() }),
-    companyEmail: Joi.string().email().when('userType', { is: 'Employer', then: Joi.required() }),
-    country: Joi.string().required()
+    companyName: Joi.when('userType', {
+        is: 'Employer',
+        then: Joi.string().required(),
+        otherwise: Joi.string().optional(),
+    }),
+    companyEmail: Joi.when('userType', {
+        is: 'Employer',
+        then: Joi.string().email().required(),
+        otherwise: Joi.string().optional(),
+    }),
+    country: Joi.string().required(),
 });
 
 
