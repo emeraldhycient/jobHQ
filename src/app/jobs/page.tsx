ab6cf29d127d3985d81884880a2f79c622a3b108
emeraldhycient <igwezehycient86@gmail.com>
2024-08-26 23:57:01 +0200

    fix: added suspense at jobs

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -3,7 +3,7 @@
 import Pagination from "@/components/dashboard/seeker/fragments/pagination";
 import RecommendedJobCard from "@/components/dashboard/seeker/fragments/recoJobCard";
 import JobSearchComponent from "@/components/dashboard/seeker/fragments/jobSearchFilter";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import jobs from "@/services/jobs";
 import SkeletonLoader from "@/components/common/skeleton/JobSkeletonLoader";
@@ -45,36 +45,38 @@ const JobSearchPage = () => {
         <section>
             <Header />
             <section className="flex flex-col space-y-4 bg-gray-5 rounded px-2 md:px-10">
-                <section className='w-full mt-8'>
-                    <JobSearchComponent onFilterChange={handleFilterChange} />
-                    {!filtersApplied ? (
-                        <h5 className='text-gray-1 text-sm font-normal mt-5 md:mt-0'>
-                            Recommended Jobs
-                        </h5>
-                    )
-                        : <h5 className='text-gray-1 text-sm font-normal mt-5 md:mt-0'>
-                            Jobs found {data?.pagination?.totalJobs}
-                        </h5>
-                    }
-                    <section className='w-full mb-4'>
-                        {isLoading ? (
-                            <SkeletonLoader count={5} />
-                        ) : (
-                            data?.jobs?.map((job: IJobItem) => (
-                                <RecommendedJobCard key={job.id} job={job} />
-                            ))
-                        )}
+                <Suspense fallback={<SkeletonLoader count={5} />}>
+                    <section className='w-full mt-8'>
+                        <JobSearchComponent onFilterChange={handleFilterChange} />
+                        {!filtersApplied ? (
+                            <h5 className='text-gray-1 text-sm font-normal mt-5 md:mt-0'>
+                                Recommended Jobs
+                            </h5>
+                        )
+                            : <h5 className='text-gray-1 text-sm font-normal mt-5 md:mt-0'>
+                                Jobs found {data?.pagination?.totalJobs}
+                            </h5>
+                        }
+                        <section className='w-full mb-4'>
+                            {isLoading ? (
+                                <SkeletonLoader count={5} />
+                            ) : (
+                                data?.jobs?.map((job: IJobItem) => (
+                                    <RecommendedJobCard key={job.id} job={job} />
+                                ))
+                            )}
+                        </section>
+                        {
+                            data && data?.pagination?.totalPages > currentPage ?
+                                <Pagination
+                                    currentPage={currentPage}
+                                    totalPages={data?.pagination?.totalPages || 1}
+                                    onPageChange={handlePageChange}
+                                />
+                                : ""
+                        }
                     </section>
-                    {
-                        data && data?.pagination?.totalPages > currentPage ?
-                            <Pagination
-                                currentPage={currentPage}
-                                totalPages={data?.pagination?.totalPages || 1}
-                                onPageChange={handlePageChange}
-                            />
-                            : ""
-                    }
-                </section>
+                </Suspense>
             </section>
         </section>
     );
